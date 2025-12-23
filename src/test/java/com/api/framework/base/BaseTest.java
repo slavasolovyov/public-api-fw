baea722da91e4d6d3672e6b6123fe78435f67615
Viacheslav <slavasolovyov@gmail.com>
2025-12-23 17:23:31 +0200

    add tests

---
+++ src/test/java/com/api/framework/base/BaseTest.java
@@ -0,0 +1,21 @@
+package com.api.framework.base;
+
+import com.api.framework.enums.Roles;
+import com.api.framework.utils.TokenStorage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.BeforeSuite;
+
+
+public class BaseTest {
+    protected static final Logger logger = LoggerFactory.getLogger(BaseTest.class);
+
+    @BeforeSuite
+    public void beforeSuite() {
+        logger.debug("Storing SUPERVISOR user token");
+        TokenStorage.setToken(Roles.SUPERVISOR, "supervisor");
+        logger.debug("Storing ADMIN user token");
+        TokenStorage.setToken(Roles.ADMIN, "admin");
+    }
+}
+
